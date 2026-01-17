0724f41459a7043b87a71d862bec230344ce4bdb
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2026-01-17 23:58:11 +0200

    add settings page

--- src/components/sidebar/upgrade.tsx
+++ src/components/sidebar/upgrade.tsx
@@ -19,7 +19,7 @@ export default function Upgrade() {
     <Button
       variant="outline"
       size="sm"
-      className="group relative ml-2 cursor-pointer overflow-hidden border-orange-400/50 bg-linear-to-r from-orange-400/10 to-pink-500/10 text-orange-400 transition-all duration-300 hover:border-orange-500/70 hover:bg-gradient-to-r hover:from-orange-500 hover:to-pink-600 hover:text-white hover:shadow-lg hover:shadow-orange-500/25"
+      className="group relative ml-2 cursor-pointer overflow-hidden border-orange-400/50 bg-linear-to-r from-orange-400/10 to-pink-500/10 text-orange-400 transition-all duration-300 hover:border-orange-500/70 hover:bg-linear-to-r hover:from-orange-500 hover:to-pink-600 hover:text-white hover:shadow-lg hover:shadow-orange-500/25"
       onClick={upgrade}
     >
       <div className="flex items-center gap-2">
